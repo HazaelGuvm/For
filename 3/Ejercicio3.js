d0f567172b03359bbd902415de19775d6c0a06e5
Marcos Hazael Guzman Vargas <168611614+HazaelGuvm@users.noreply.github.com>
2024-07-17 16:30:07 -0600

    Ejercicio3.js

--- 3/Ejercicio3.js
+++ 3/Ejercicio3.js
@@ -1,11 +1,8 @@
-// Definimos el arreglo
-let arreglo = [4, "dos", 8, "tres", 5, 9, 1, "cero"];
 
-// Iteramos sobre el arreglo usando un bucle for
+let arreglo = [4, "dos", 8, "tres", 5, 9, 1, "cero"]
+
 for (let i = 0; i < arreglo.length; i++) {
-    // Verificamos si el elemento en la posición i es un número
-    if (typeof arreglo[i] === 'number') {
-        // Mostramos el elemento en consola si es un número
-        console.log(arreglo[i]);
+  if (typeof arreglo[i] === 'number') {
+        console.log(arreglo[i])
     }
 }
